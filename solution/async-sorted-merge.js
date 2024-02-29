a91bed2c8b443e12a8603959608bc5d6ff448ded
Shahar Sharron <shaharsharron@gmail.com>
2024-02-28 18:03:07 -0800

    naive approach to part 2

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,50 @@
-"use strict";
+'use strict';
+
+const MinHeap = require('../lib/min-heap');
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
+const handleItemsInHeap = (minHeap, printer) => {
+	if (minHeap.isEmpty()) {
+		return Promise.resolve();
+	}
+
+	const minLogEntryDataItem = minHeap.extractMin();
+	printer.print(minLogEntryDataItem.item);
+
+	return minLogEntryDataItem.source
+		.popAsync()
+		.then((nextItemInSource) => {
+			if (nextItemInSource) {
+				minHeap.insert({
+					item: nextItemInSource,
+					source: minLogEntryDataItem.source,
+				});
+			}
+		})
+		.then(() => {
+			handleItemsInHeap(minHeap, printer);
+		});
+};
+
 module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
+	return new Promise((resolve, reject) => {
+		const minHeap = new MinHeap();
+
+		// Initialize the heap with the first item from each logSource
+		const initialPromises = logSources.map((logSource) => {
+			return logSource
+				.popAsync()
+				.then(
+					(item) =>
+						item && minHeap.insert({ item, source: logSource })
+				);
+		});
+
+		Promise.all(initialPromises).then(() => {
+			handleItemsInHeap(minHeap, printer);
+			printer.done();
+			resolve(console.log('Async sort complete.'));
+		});
+	});
 };
