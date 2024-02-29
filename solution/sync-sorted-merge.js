65f66afc9e9eb006ed8ae7cf834126eabc6be53e
Shahar Sharron <shaharsharron@gmail.com>
2024-02-28 17:36:52 -0800

    finish part 1

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,34 @@
-"use strict";
+'use strict';
+
+const MinHeap = require('../lib/min-heap');
 
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+	const minHeap = new MinHeap();
+
+	// Initialize the heap with the first item from each logSource
+	logSources.forEach((logSource) => {
+		const item = logSource.pop();
+		if (item !== null) {
+			minHeap.insert({ item, source: logSource });
+		}
+	});
+
+	// Continue until the min heap is empty
+	while (!minHeap.isEmpty()) {
+		const minLogEntryDataItem = minHeap.extractMin();
+		printer.print(minLogEntryDataItem.item);
+
+		const nextItemInSource = minLogEntryDataItem.source.pop();
+		if (nextItemInSource) {
+			minHeap.insert({
+				item: nextItemInSource,
+				source: minLogEntryDataItem.source,
+			});
+		}
+	}
+
+	printer.done();
+	return console.log('Sync sort complete.');
 };
